f19904f82e4741af1d824dbff3570aa2d12bef9c
Terence Buchannan <terencebuchannan@gmail.com>
2017-05-03 02:08:40 -0400

    Refactor functions

--- js/DOMination.js
+++ js/DOMination.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  window.$l('.addItemButton').on('click', () => {
+  $l('.addItemButton').on('click', () => {
 
-    let list = window.$l('ul');
-    let input = window.$l('.addItemInput').els[0].value;
+    let list = $l('ul');
+    let input = $l('.addItemInput').els[0].value;
     if ( input.length > 0 ) {
       list.append(
         "<li class='items'>" + input +
         " <button class='button removeItem'>Remove Item</button>" +
         "</li>");
 
-      window.$l('.addItemInput').els[0].value = '';
+      $l('.addItemInput').els[0].value = '';
     }
 
-    window.$l(".removeItem").on('click', (e) => {
-      window.$l(e.currentTarget).parent().els[0].remove();
+    $l(".removeItem").on('click', (e) => {
+      $l(e.currentTarget).parent().els[0].remove();
     });
   });
 });
