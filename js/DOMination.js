7af0e48dcc7e0d617ba7c711c25201b8aad70dd3
Terence Buchannan <terencebuchannan@gmail.com>
2017-05-03 00:19:30 -0400

    Add jQuery logic

---
+++ js/DOMination.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  window.$l('.addItemButton').on('click', () => {
+
+    let list = window.$l('ul');
+    let input = window.$l('.addItemInput').els[0].value;
+    if ( input.length > 0 ) {
+      list.append(
+        "<li>" + input +
+        " <button class='removeItem'>Remove Item</button>" +
+        "</li>");
+
+      window.$l('.addItemInput').els[0].value = '';
+    }
+
+    window.$l(".removeItem").on('click', (e) => {
+      window.$l(e.currentTarget).parent().els[0].remove();
+    });
+  });
+});
