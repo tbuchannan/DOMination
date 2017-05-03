fefc69668f44735b96862d99ab4c740dff3591e9
Terence Buchannan <terencebuchannan@gmail.com>
2017-05-03 01:14:07 -0400

    Add styling

--- js/DOMination.js
+++ js/DOMination.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   window.$l('.addItemButton').on('click', () => {
 
     let list = window.$l('ul');
     let input = window.$l('.addItemInput').els[0].value;
     if ( input.length > 0 ) {
       list.append(
-        "<li>" + input +
-        " <button class='removeItem'>Remove Item</button>" +
+        "<li class='items'>" + input +
+        " <button class='button removeItem'>Remove Item</button>" +
         "</li>");
 
       window.$l('.addItemInput').els[0].value = '';
